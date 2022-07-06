d2f8ed79865a5642bc728c8c022e47677cebba1e
EllisWitten <e.witten.2001@gmail.com>
2022-07-06 11:03:38 +0100

    06/07/2022 git upload

--- notes/VS-Introduction/VS-Introduction/Program.cs
+++ notes/VS-Introduction/VS-Introduction/Program.cs
@@ -4,13 +4,19 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("I have gained sentience");
+        /*Console.WriteLine("I have gained sentience");
         int x = 100;
         x += 10;
         for (int i = 0; i < 10; i++)
         {
             Console.WriteLine(i);
-            x += 1;
+            x += 1;*/
+
+        Console.WriteLine("Writing out command line arguments");
+        foreach (string item in args)
+        {
+            Console.WriteLine(item);
+        }
         }
     }
 }
